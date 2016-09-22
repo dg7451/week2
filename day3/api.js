92372b8766236b69d9c965150043cc150b41ffab
Darwin Goble <darwin.goble@interapthq.com>
2016-09-22 04:46:50 -0400

    update

---
+++ day3/api.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<header> 
+<title> HTML Service Request </title>
+</header>
+
+<body> var=p;
+
+<form action url="https://search.yahoo.com?p=weather&results" method = GET>
+    
+input type=text name = "p";
+<button>Search</button>
+    
+</form>  
+
+
+
+
+</body>
+
+</html>
+
+/* PROTOCOL: https
+Path:
+port:   not in this order correct would be??
+scheme:
+host:
+QueryString: p=weather
--- week2proj/weatherprojdg/weather.js
+++ week2proj/weatherprojdg/weather.js
@@ -1,18 +1,32 @@
 
-var temp;
-var loc;
-var icon;
-var humidity;
-var wind;
-var direction;
+var  id="temp";
+var id="loc";
+var id="icon";
+var  id="humidity";
+var  id="wind";
+var id="direction";
+var id="mintemp";
+var id="maxtemp";
+var id="lat";
+var id="lng";
+
+$(function () {
+
+    var url = "http://rest.learncode.academy/api/learncode/friends"
+    $.ajax("http://rest.learncode.academy/api/learncode/friends/57e116b91f6147010025553b").done(function (data) 
+    {
+        $("#me").html(data.name + data.drink );
+        
+    });
+    });
 
 window.onload = function() {
-    temp = document.getElementById("temperature");
-    loc  = document.getElementById("location");
-    icon = document.getElementById("icon");
-    humidity  = document.getElementById("humidity");
-    wind  = document.getElementById("wind");
-    direction = document.getElementById("direction");
+    "temp"" = document.getElementById("temperature");
+    "loc""  = document.getElementById("loc");
+    "icon" = document.getElementById("icon");
+    "humidity"  = document.getElementById("humidity");
+    "wind"  = document.getElementById("wind");
+    "direction" = document.getElementById("direction");
     
 }
 
