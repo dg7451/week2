bf6d59d5895f274134830e9b3f8ae75c0cab9082
Darwin Goble <darwin.goble@interapthq.com>
2016-09-22 06:30:43 -0400

    updates

--- week2proj/weatherprojdg/weather.js
+++ week2proj/weatherprojdg/weather.js
@@ -12,13 +12,10 @@ var id="lng";
 
 $(function () {
 
-    var url = "http://rest.learncode.academy/api/learncode/friends"
-    $.ajax("http://rest.learncode.academy/api/learncode/friends/57e116b91f6147010025553b").done(function (data) 
-    {
-        $("#me").html(data.name + data.drink );
-        
-    });
-    });
+    var url = var DarkSkyKey = "8bd2541890ff80a0cfa7628955f6de1b ";
+   $.ajax("https://api.darksky.net/forecast/" + "DarkSkyKey" + "/37.8267,-122.4233", { dataType: "jsonp"}).done(function(data) {
+       console.log(data); 
+     });
 
 window.onload = function() {
     "temp"" = document.getElementById("temperature");
