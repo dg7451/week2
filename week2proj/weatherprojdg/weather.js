efec83d71178223523e8699db66884e07473b872
Darwin Goble <darwin.goble@interapthq.com>
2016-09-23 04:18:07 -0400

    ssaving updates

--- week2proj/weatherprojdg/weather.js
+++ week2proj/weatherprojdg/weather.js
@@ -1,30 +1,28 @@
 
-var  id="temp";
-var id="loc";
-var id="icon";
-var  id="humidity";
-var  id="wind";
-var id="direction";
-var id="mintemp";
-var id="maxtemp";
-var id="lat";
-var id="lng";
 
 $(function () {
 
-    var url = var DarkSkyKey = "8bd2541890ff80a0cfa7628955f6de1b ";
+    var zipCode = 41635;
+
+    var googleurl="https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + "&key=AIzaSyDMzCizTxJbFhlR9kiyIWS_8GT_Nc8yV3Q";
+    var long =
+    var lat=
+
+    var  DarkSkyKey = "8bd2541890ff80a0cfa7628955f6de1b ";
    $.ajax("https://api.darksky.net/forecast/" + "DarkSkyKey" + "/37.8267,-122.4233", { dataType: "jsonp"}).done(function(data) {
        console.log(data); 
      });
 
-window.onload = function() {
-    "temp"" = document.getElementById("temperature");
-    "loc""  = document.getElementById("loc");
-    "icon" = document.getElementById("icon");
-    "humidity"  = document.getElementById("humidity");
-    "wind"  = document.getElementById("wind");
-    "direction" = document.getElementById("direction");
-    
+      //alternative: $("button#add").on("click", function(ev) {
+    $("button#add").click(function(ev /*event*/) {
+        var zip-code = $("#zipcode").val();
+
+
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
 }
 
 
